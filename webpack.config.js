4036ab2e910ef236558d56979641635d036157a9
Franco Gotusso <gotusso@gmail.com>
2018-04-14 12:00:12 +0100

    Copy static files to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/main.js',
+    plugins: [
+      new CopyWebpackPlugin([
+        { from: 'src/**/*', ignore: [ '*.js' ], flatten: true }
+      ]),
+    ],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
